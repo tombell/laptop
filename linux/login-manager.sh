8a4c9658176cc7c8884ad10565ddf06c4ed8633f
Tom Bell <tomb@tombell.dev>
2026-02-13 20:02:36 +0000

    Add linux script for setting up greetd login manager

---
+++ linux/login-manager.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "==> Setting up greetd login manager…"
+
+if ! grep -qi "hyprland" "/etc/greetd/config.toml"; then
+  sudo tee /etc/greetd/config.toml <<EOF >/dev/null
+[terminal]
+vt = 1
+
+[default_session]
+command = "agreety --cmd /usr/bin/zsh"
+user = "greeter"
+
+[initial_session]
+command = "uwsm start -- hyprland.desktop"
+user = "tombell"
+EOF
+fi
+
+sudo systemctl enable greetd.service
