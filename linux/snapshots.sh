a97c6e644125d1b5d4e9fadbd5e9a9f111c14a83
Tom Bell <tomb@tombell.dev>
2026-02-13 19:59:54 +0000

    Add linux script for setting up btrfs snapshots

---
+++ linux/snapshots.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "==> Setting up btrfs snapshots…"
+
+yay -S --noconfirm --needed --removemake limine-snapper-sync snap-pac
+
+if ! sudo snapper list-configs 2>/dev/null | grep -q "root"; then
+  sudo snapper -c root create-config /
+fi
+
+if ! sudo snapper list-configs 2>/dev/null | grep -q "home"; then
+  sudo snapper -c home create-config /home
+fi
+
+sudo sed -i 's/^TIMELINE_CREATE="yes"/TIMELINE_CREATE="no"/' /etc/snapper/configs/{root,home}
+sudo sed -i 's/^NUMBER_LIMIT="50"/NUMBER_LIMIT="5"/' /etc/snapper/configs/{root,home}
+sudo sed -i 's/^NUMBER_LIMIT_IMPORTANT="10"/NUMBER_LIMIT_IMPORTANT="5"/' /etc/snapper/configs/{root,home}
+
+sudo systemctl enable --now limine-snapper-sync.service
