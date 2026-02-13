dd59ec66f4dfe21476b648c55a60606b55dfc859
Tom Bell <tomb@tombell.dev>
2026-02-13 19:03:31 +0000

    Add common script for setting up dotfiles with rcm

---
+++ common/rcm.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+setup_dotfiles() {
+  local tags=()
+  local extra_args=()
+
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+    --)
+      shift
+      break
+      ;;
+    *)
+      tags+=("$1")
+      shift
+      ;;
+    esac
+  done
+
+  extra_args=("$@")
+
+  local tag_args=()
+  for tag in "${tags[@]}"; do
+    tag_args+=("-t" "$tag")
+  done
+
+  echo "==> Setting up dotfiles with rcm…"
+
+  rcup -d "${HOME}/.dotfiles" \
+    "${tag_args[@]}" \
+    -x LICENSE -x README.md -x scripts \
+    "${extra_args[@]}"
+}
