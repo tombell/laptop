9debee74384e904e0c4e4c686904b76ca3740c7e
Tom Bell <tomb@tombell.dev>
2026-02-13 20:07:26 +0000

    Add linux setup script

---
+++ linux.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+source "$(dirname "$0")/common/rcm.sh"
+source "$(dirname "$0")/linux/pacman.sh"
+source "$(dirname "$0")/linux/aur.sh"
+source "$(dirname "$0")/linux/packages.sh"
+
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone https://github.com/tombell/dotfiles.git "$HOME/.dotfiles"
+fi
+
+setup_dotfiles linux
+
+source "$(dirname "$0")/linux/bootloader.sh"
+source "$(dirname "$0")/linux/snapshots.sh"
+source "$(dirname "$0")/linux/fonts.sh"
+source "$(dirname "$0")/linux/login-manager.sh"
+source "$(dirname "$0")/linux/shell.sh"
+source "$(dirname "$0")/linux/gui.sh"
+source "$(dirname "$0")/common/ssh.sh"
+source "$(dirname "$0")/common/mise.sh"
