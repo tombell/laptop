af7c6b205c516ead4403441e91eddd17974a7ea4
Tom Bell <tomb@tombell.dev>
2026-02-13 19:05:51 +0000

    Add common script to setup personal SSH keys

---
+++ common/ssh.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+mkdir -p ~/.ssh
+
+echo "==> Setting up SSH keys…"
+
+[ -e "~/.ssh/Personal.pub" ] || op read "op://Personal/Personal/public key" >~/.ssh/Personal.pub
+[ -e "~/.ssh/Personal" ] || op read "op://Personal/Personal/private key" >~/.ssh/Personal
+
+chmod 600 ~/.ssh/Personal
+chmod 600 ~/.ssh/Personal.pub
