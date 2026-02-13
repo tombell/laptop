bbdac20c765a8a3e0fa2e26072deae52a14e9e1b
Tom Bell <tomb@tombell.dev>
2026-02-13 19:17:28 +0000

    Add macos setup scripts for personal and work laptops

---
+++ work.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "==> Setting up work macOS laptop…"
+
+source "$(dirname "$0")/common/rcm.sh"
+source "$(dirname "$0")/macos/homebrew.sh"
+
+if [ ! -d "$HOME/.dotfiles" ]; then
+  git clone https://github.com/tombell/dotfiles.git "$HOME/.dotfiles"
+fi
+
+setup_dotfiles macos work -- -S karabiner
+
+# TODO: log into 1password here so we can get SSH keys
+
+source "$(dirname "$0")/macos/defaults.sh"
+source "$(dirname "$0")/common/ssh.sh"
+source "$(dirname "$0")/common/mise.sh"
