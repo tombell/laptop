a3086e3cb0f379186bbd25c6e12a545b526907af
Tom Bell <tomb@tombell.dev>
2026-08-17 17:08:03 +0100

    Remove herdr-navigator plugin, not actively using it

--- common/herdr.sh
+++ common/herdr.sh
@@ -18,5 +18,5 @@ install_herdr_plugin() {
   herdr plugin install "$repository" "$@" --yes
 }
 
-install_herdr_plugin herdr-navigator thanhdat77/herdr-navigator --ref v0.3.5
+# install_herdr_plugin herdr-navigator thanhdat77/herdr-navigator --ref v0.3.5
 install_herdr_plugin mroth.jj-status mroth/herdr-jj-status
