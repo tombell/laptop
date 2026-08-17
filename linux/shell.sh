786351eaec1455f9313679d099fc83e036e60749
Tom Bell <tomb@tombell.dev>
2026-08-17 20:19:07 +0100

    Set Linux shell to fish

--- linux/shell.sh
+++ linux/shell.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [ "${SHELL:-}" != "/usr/bin/zsh" ]; then
-  echo "==> Setting shell to zsh…"
-  chsh -s /usr/bin/zsh
+if [ "${SHELL:-}" != "/usr/bin/fish" ]; then
+  echo "==> Setting shell to fish…"
+  chsh -s /usr/bin/fish
 fi
