c0244f25889d764e023f3733b0752e473ea5dcfe
Tom Bell <tomb@tombell.dev>
2026-09-18 19:29:50 +0100

    Configure Raspberry Pi command-line environment

--- profiles/rpi.sh
+++ profiles/rpi.sh
@@ -7,6 +7,7 @@ setup_laptop_root
 source "$ROOT_DIR/common/rcm.sh"
 
 source "$ROOT_DIR/linux/debian/packages.sh"
+source "$ROOT_DIR/linux/shared/shell.sh"
 
 ensure_dotfiles
 # Install base dotfiles without editor, agent, Pi, or SSH configuration.
